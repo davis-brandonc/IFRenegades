ddfda8c47ac273f9723d7a7dd69c4749e3dbc31f
Davis-Brandon <bdavis29@byui.edu>
2020-05-23 17:19:37 -0600

    Create schedule.js

---
+++ practices/json/schedule.js
@@ -0,0 +1,42 @@
+{
+  "schedule" : [
+  {
+    "name": "Idaho Falls",
+    "address": [
+	"1000 Memorial Drive",
+	"Idaho Falls, ID 83402-3410"
+	],
+    "telephoneNumber": "208-522-7669",
+    "services": [
+	"Clothing rental available",
+	"Cafeteria available",
+	"No patron housing available",
+	"Distribution center nearby"
+	],
+    "templeHours": "Tuesday - Saturday ()",
+    "baptismSchedule": [
+    "Tuesday - Friday (6:00 AM - 8:30 PM)",
+	"Saturday (6:30 AM - 6:00 PM)"],
+    "initiatorySchedule": [
+    "Tuesday - Friday (6:30 AM - 8:30 PM)",
+	"Saturday (6:00 AM - 5:30 PM)"
+    ],
+    "endowmentSchedule": [
+    "Tuesday - Friday (6:30 AM - 8:00 PM)",
+	"Saturday (6:00 AM - 5:00 PM)"
+    ],
+    "sealingSchedule": [
+    "Tuesday - Friday (6:00 AM - 7:40 PM)",
+	"Saturday (6:00 AM - 6:05 PM)"
+    ],
+    "templeClosures": [
+    "Monday, 24 June 2019 - Monday, 8 July 2019",
+	"Saturday, 5 October 2019",
+	"Wednesday, 27 November 2019 (Limited Hours)",
+	"Thursday, 28 November 2019",
+	"Tuesday, 24 December 2019 - Wednesday, 25 December 2019",
+	"Tuesday, 31 December 2019"
+    ]
+  }
+]
+      
